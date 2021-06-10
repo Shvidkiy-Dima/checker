ba1d9ee8259389a864076c145411caace1c88bec
d <d.shvidkiy@spider.ru>
2021-06-10 14:11:13 +0300

    add host

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -2,6 +2,6 @@ from .base import *
 
 DEBUG = False
 
-API_HOST = 'http://localhost'
-SITE_HOST = 'http://localhost'
+API_HOST = 'http://194.163.142.64'
+SITE_HOST = 'http://194.163.142.64'
 LOGIN_PAGE = SITE_HOST + '/dashboard/login/'
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost/';
+axios.defaults.baseURL = 'http://194.163.142.64/';
 
 ReactDOM.render(
   <React.StrictMode>
