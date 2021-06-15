09a973f08cfe105e0b8bb78cff2f9a95997a8aed
d <d.shvidkiy@spider.ru>
2021-06-15 11:23:53 +0300

    fix docker start

--- compose/celery/start.sh
+++ compose/celery/start.sh
@@ -4,6 +4,6 @@
 python /app/check_conn.py --service-name redis --port 6379  --ip redis
 python /app/check_conn.py --service-name db --port 5432  --ip db
 python /app/check_conn.py --service-name rabbit --port 5672  --ip rabbit
-
+python /app/check_conn.py --service-name django --port 5000  --ip django
 
 celery -A project worker -l info
--- compose/notifier/start.sh
+++ compose/notifier/start.sh
@@ -5,7 +5,6 @@ sleep 2
 python /app/check_conn.py --service-name redis --port 6379  --ip redis
 python /app/check_conn.py --service-name db --port 5432  --ip db
 python /app/check_conn.py --service-name rabbit --port 5672  --ip rabbit
+python /app/check_conn.py --service-name django --port 5000  --ip django
 
-python /app/manage.py makemigrations
-python /app/manage.py migrate --fake-initial
 python /app/background_service/notifier/runner.py
