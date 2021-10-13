4fe39101012456b8ebf14b6f04d465f14852ef8a
Borisov Dima <shvidkiy.dmitriy@gmail.com>
2021-10-13 15:21:02 +0300

    fix compose

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -2,6 +2,7 @@ from .base import *
 
 DEBUG = False
 
-API_HOST = 'http://45.84.226.13'
-SITE_HOST = 'http://45.84.226.13'
+API_HOST = 'https://45.84.226.13'
+SITE_HOST = 'https://45.84.226.13'
 LOGIN_PAGE = SITE_HOST + '/login/'
+
