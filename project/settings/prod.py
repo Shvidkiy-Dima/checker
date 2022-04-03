978b19419c2e3bdf9621cdfa814a1737bf20f86c
Dima B <dima@example.com>
2022-04-03 15:18:30 +0300

    fix host

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-API_HOST = 'https://check-it-out.fun'
-SITE_HOST = 'https://check-it-out.fun'
+API_HOST = 'https://check-it-out.site'
+SITE_HOST = 'https://check-it-out.site'
 LOGIN_PAGE = SITE_HOST + '/d/#/login'
 
